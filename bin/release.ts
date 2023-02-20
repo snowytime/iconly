62dbb2308baf568cc81d757bb9d881e8dc046a2e
Snaer <87044139+snowytime@users.noreply.github.com>
2023-02-19 20:45:11 -0700

    ⚡️ auto patch

--- bin/release.ts
+++ bin/release.ts
@@ -1,5 +1,8 @@
 import { exec } from "node:child_process";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 import { promisify } from "node:util";
+import { inc } from "semver";
 
 const exec_promise = promisify(exec);
 
@@ -10,18 +13,22 @@ export enum ReleaseEnum {
 }
 
 export async function release(release: ReleaseEnum) {
+    const currentVersion = JSON.parse(
+        await readFile(join(process.cwd(), "react/package.json"), "utf8"),
+    ).version;
+    const upcomingVersion = inc(currentVersion, release);
     try {
         // begin by incrementing packages properly
         await exec_promise(`cd react && npm version ${release}`);
         await exec_promise(`cd vue && npm version ${release}`);
         // finish off git
         await exec_promise(
-            `git add . && git commit -m '🥳 auto ${release}' && git push origin master`,
+            `git add . && git commit -m '⚡️ auto ${release}' && git push origin master`,
         );
         // release each package
         await exec_promise("cd react && npm publish");
         await exec_promise("cd vue && npm publish");
-        console.log(`✅ ${release} successful`);
+        console.log(`✅ ${release}. ${currentVersion} -> ${upcomingVersion}`);
     } catch (e) {
         console.log(e);
         console.log(`❌ Error performing release.`);
